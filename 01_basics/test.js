147b33f7d42e7e16fb3d002a618958fb505cc2e9
Snehasree <snehabrsc@gmail.com>
2025-01-19 16:05:39 +0000

    modified

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
-console.log("Successfully container configuration done!!!")
+// console.log("Successfully container configuration done!!!")
+
+
+let user1={
+    emailId:"one@gmail.com",
+    age: 22
+}
+
+let user2=user1
+user2.age=23
+
+
+console.log(user1,user2)
