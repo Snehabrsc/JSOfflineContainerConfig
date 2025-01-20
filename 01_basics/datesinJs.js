7b86304f341cbee79ab66c9a26dff9df14b07337
Snehasree <snehabrsc@gmail.com>
2025-01-20 14:38:11 +0000

    updated

---
+++ 01_basics/datesinJs.js
@@ -0,0 +1,23 @@
+
+
+let newDate=new Date()
+
+// console.log(newDate.toDateString())
+
+// console.log(newDate.toLocaleDateString())
+
+// console.log(typeof(newDate));
+
+// const mydate=new Date("01-20-2025")
+
+// console.log(mydate.toLocaleString())
+
+// console.log(mydate.getTime())
+
+// console.log(Date.now())
+
+//console.log(Math.floor(Date.now()/1000))
+
+let anothrDate=new Date()
+
+console.log(anothrDate.getDay())
