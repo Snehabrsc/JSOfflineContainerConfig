26e36aec358ddd910d24e30e59996e34cb519531
Snehasree <snehabrsc@gmail.com>
2025-01-20 18:24:26 +0000

    array updated

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -9,10 +9,42 @@ const myArr= new Array(1,2,3,"a")
 
     // console.log(myArr);
 
-    console.log(myArr.pop());
-    console.log(myArr);
+    // console.log(myArr.pop());
+    // console.log(myArr);
 
-      console.log(myArr.pop());
-    console.log(myArr);
+    //   console.log(myArr.pop());
+    // console.log(myArr);
+
+
+
+
+
+
+    // console.log(myArr.unshift(4))
+    // console.log(myArr)
+
+    // console.log(myArr.shift());
+    // console.log(myArr)
     
+    // console.log(myArr.includes("p"));
+    // console.log(myArr);
+
+    // console.log(myArr.indexOf(9))
+    
+    
+
+// const newArr=myArr.join()
+// console.log(myArr)
+// console.log(newArr)
+
+
+
+console.log(myArr.slice(0,2))
+console.log(myArr)
+
+
+console.log(myArr.splice(0,2))
+console.log(myArr)
+
+
 
