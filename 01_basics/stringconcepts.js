399dfb136f4ab30a3bd0213eb495bfaaa6f1d642
Snehasree <snehabrsc@gmail.com>
2025-01-20 12:48:44 +0000

    modified

--- 01_basics/stringconcepts.js
+++ 01_basics/stringconcepts.js
@@ -1,6 +1,32 @@
 const repocount=4
 const name="sneha"
 
-console.log(`Hello my name is  ${name} and my repocount is ${repocount}`);
+//console.log(`Hello my name is  ${name} and my repocount is ${repocount}`);
 
-const gameName=new String('')
+const gameName=new String('githubnew')
+
+// console.log(gameName[0]);
+
+// console.log(gameName.charAt(4));
+
+// console.log(gameName.indexOf('i'))
+
+// const newSubstring=gameName.substring(0,2)
+// console.log(newSubstring)
+
+
+// const newString=gameName.slice(-10,3)
+
+// console.log(newString);
+
+// const newName="    hello    world"
+
+// const trimString=newName.trim()
+
+// console.log(trimString);
+
+// const replaceString=trimString.replace('hello','My')
+// console.log(replaceString)
+
+
+console.log(gameName.includes("git"))
