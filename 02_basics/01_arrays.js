c0d7a15172404da6d7f188afedcd626b205b5a43
Snehasree <snehabrsc@gmail.com>
2025-01-20 16:43:19 +0000

    updated

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,18 @@
+
+
+const myArr= new Array(1,2,3,"a")
+    // console.log(myArr[2]);
+    
+    //Array methods
+
+    // console.log(myArr.push(5))
+
+    // console.log(myArr);
+
+    console.log(myArr.pop());
+    console.log(myArr);
+
+      console.log(myArr.pop());
+    console.log(myArr);
+    
+
