b0b09fd36ef5ae6feb6e452a86fab6d1645017a5
Snehasree <snehabrsc@gmail.com>
2025-01-20 18:46:39 +0000

    udated new array methods

---
+++ 02_basics/02_array.js
@@ -0,0 +1,28 @@
+const marvel_heros=["thor","Ironman","spiderman"]
+const dc_heros=["superman","flash","batman"]
+
+
+
+// const allHeros=marvel_heros.concat(dc_heros)
+// console.log(allHeros)
+
+// const allNewheros=[...marvel_heros,...dc_heros]
+// console.log(allNewheros)  ///spread operator
+
+
+const flatArray=[1,2,["a","b"],[5,7,[8,9,3]]]
+
+//console.log(flatArray.flat(Infinity))
+
+
+// console.log(Array.isArray("hjdsgddgh"))
+// console.log(Array.from("dhfgfhdhg"))
+
+
+
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3))
