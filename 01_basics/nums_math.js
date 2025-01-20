7001bac2ea7dd99f9d721d5834ec9f173e677c2d
Snehasree <snehabrsc@gmail.com>
2025-01-20 14:00:53 +0000

    updated file

--- 01_basics/nums_math.js
+++ 01_basics/nums_math.js
@@ -18,4 +18,26 @@ console.log(total)
 
 //******************Maths******************* */
 
+// console.log(Math)
+
+// console.log(Math.abs(-8))
+// console.log(Math.round(4.36))
+
+// console.log(Math.ceil(4.9))
+
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(4,6,2,9))
+
+// console.log(Math.max(45,88,11))
+
+
+// console.log(Math.random())
+// console.log(Math.random()*10) //returns a value between 0 and 1
+
+
+const minNum=25
+const maxNum=75
+
+console.log(Math.floor(Math.random() * (maxNum-minNum +1 ))+minNum) ////Very imp If want any value between specified number
 
