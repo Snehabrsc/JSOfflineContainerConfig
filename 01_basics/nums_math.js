1249d9a6fcb6eae4fa36032f410c599b5ff7a944
Snehasree <snehabrsc@gmail.com>
2025-01-20 13:29:16 +0000

    modified

---
+++ 01_basics/nums_math.js
@@ -0,0 +1,21 @@
+const total=new Number(225);
+console.log(total)
+
+// console.log(total.toString());
+
+// console.log((total.toFixed(2)));
+
+
+//  const newNum=new Number(145.8499)
+//  console.log(newNum.toPrecision(4))
+
+
+// const meNum=new Number(5000000000045)
+
+// console.log(meNum.toLocaleString("en-IN"))
+
+
+
+//******************Maths******************* */
+
+
