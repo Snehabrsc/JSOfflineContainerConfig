48b7362ab3e6e03a47d6feba62cb80631dd8dc45
Snehasree <snehabrsc@gmail.com>
2025-01-21 09:23:22 +0000

    updated v v imp

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -23,12 +23,41 @@ function funName(){
 
 // console.log(loginMsg('sam'))
 
-function loginTime(name,logintime){
-    if(!name){
-        console.log("please enter a user name")
-        return
-    }
-    return `${name} loggin time at ${logintime}` 
+// function loginTime(name,logintime){
+//     if(!name){
+//         console.log("please enter a user name")
+//         return
+//     }
+//     return `${name} loggin time at ${logintime}` 
+// }
+
+// console.log(loginTime('',Date("10-4-2025"))) 
+
+function calcartprice(...num1){
+    
+    return num1
 }
 
-console.log(loginTime('',Date("10-4-2025"))) 
+//console.log(calcartprice(1,2,3))
+
+const userdetails={
+    name: "Robert",
+    age : 44,
+    location :"USA"
+}
+
+function useraccess(anyObj){
+    return `Name is ${anyObj.name} and Location is ${anyObj.location}`
+}
+
+console.log(useraccess(userdetails))
+
+const costitem=[112,230,229]
+
+function getcostitem(getarray){
+    return `cost of this item is ${getarray[1]}`
+}
+
+console.log(getcostitem(costitem))
+
+
