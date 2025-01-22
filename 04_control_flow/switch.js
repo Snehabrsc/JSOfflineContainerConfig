526ffb80b5fd4a0126e6b30ae68e97483bceb4d2
Snehasree <snehabrsc@gmail.com>
2025-01-22 18:04:10 +0000

    truthy and falsy value

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+const month=[1,2,3]
+
+
+switch (month) {
+    case month[0]=1:
+        console.log("January")
+        break;
+        case month[1]= 2:
+        console.log("feb")
+        break;
+        case month[2]=3:
+        console.log("march")
+        break;
+
+
+    default:
+        console.log("default")
+        break;
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,39 @@
+const userEmail=[]
+ 
+if(userEmail){
+    console.log("got mail");
+    
+}else{
+    console.log("dint get email")
+}
+
+
+
+
+if (userEmail.length===0) {
+    console.log("array is empty")
+    
+}
+
+
+const emptyobj={}
+
+
+
+    if (Object.values(emptyobj).length===0) {
+        console.log("empty object");
+                
+    }
+
+
+
+
+
+
+//falsy value
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Truthy value
+
+// [], "0", 'false', " ", {}, function(){}
