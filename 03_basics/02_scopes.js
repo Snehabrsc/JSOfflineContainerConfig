1e2f71c80f566d7b18cfd2b660509b2a5eabe717
Snehasree <snehabrsc@gmail.com>
2025-01-22 07:48:35 +0000

    scopes updated

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,31 @@
+function one(){
+    const username="sober"
+
+    function two(){
+        const website="safari"
+        console.log(username)
+    }
+
+    // console.log(website)
+    two()
+}
+
+one()
+
+//////++++Important+++++/////
+
+
+
+function subone(num){
+    return num-1
+}
+
+console.log(subone(5))
+
+const retval=function(num){
+    return num*3
+}
+
+console.log(retval(2))
+
+
