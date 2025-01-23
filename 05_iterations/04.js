33583be1c713d127576320e6d79dfd09ed843ab0
Snehasree <snehabrsc@gmail.com>
2025-01-23 08:33:36 +0000

    edited

--- 05_iterations/04.js
+++ 05_iterations/04.js
@@ -1,18 +1,38 @@
- const myObj={
-    js:"javascript",
-    cpp :"c++",
-    rb:"rubby",
-    swift:"swift by apple"
- }
+//  const myObj={
+//     js:"javascript",
+//     cpp :"c++",
+//     rb:"rubby",
+//     swift:"swift by apple"
+//  }
 
- for (const key in myObj) {
-    //console.log(key,myObj[key]);
- }
-
-
- const programarray=["js","c++","rubby","javascript"]
+//  for (const key in myObj) {
+//     //console.log(key,myObj[key]);
+//  }
 
 
- for (const key in programarray) {
-    console.log(programarray[key])
- }
+//  const programarray=["js","c++","rubby","javascript"]
+
+
+//  for (const key in programarray) {
+//     console.log(programarray[key])
+//  }
+
+
+const arrpage=[1,2,3,4]
+
+for (const key in arrpage) {
+  // console.log(arrpage[key]);
+   
+}
+
+
+const objflower={
+   name :"daisy",
+   season:"winter",
+   color:"white"
+}
+
+for (const elem in objflower) {
+   console.log(elem,objflower[elem])
+   
+}
