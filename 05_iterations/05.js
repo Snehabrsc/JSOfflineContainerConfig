0254559ad7723037af4d096d6879fedbdef10caf
Snehasree <snehabrsc@gmail.com>
2025-01-23 09:23:00 +0000

    filter practice

--- 05_iterations/05.js
+++ 05_iterations/05.js
@@ -37,6 +37,30 @@ const mycoding=[
 
 
 mycoding.forEach( (val) => {
-    console.log(val.langfile)
-    console.log(val.langname)
-}  )
+    // console.log(val.langfile)
+    // console.log(val.langname)
+}  ) 
+
+
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+
+const finalnum=myNums.filter((num) => num>9 )
+// console.log(finalnum)
+
+
+const userbook=book.filter( (bk)=> bk.genre==="history" )
+
+console.log(userbook)
+
+const publishbook=book.filter( (bkp) => bkp.publish >2000 && bkp.genre==="history" )
+
+console.log(publishbook)
+
+
+
+
+
+
+
+
+
